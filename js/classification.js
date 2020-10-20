2bc686bb47fcc2c646bfa413a59af38004fb90c7
Ann Sizemore <annsize@seas.upenn.edu>
2020-10-20 18:55:12 -0400

    added comparison plot

--- js/classification.js
+++ js/classification.js
@@ -4,6 +4,11 @@
         // and https://bl.ocks.org/dimitardanailov/99950eee511375b97de749b597147d19
 
         svg = d3.select("#svg0");
+        svg1 = d3.select("#svg-pair1")
+        svg2 = d3.select("#svg-pair2")
+        const svg_pair_s = +svg1.attr("width")   // Both paired svgs are squares so we only need one side.
+        console.log(svg_pair_s)
+
 
         const width = +svg.attr("width"),
             height = +svg.attr("height"),
@@ -20,9 +25,30 @@
             .interpolator(d3.interpolateReds);
 
 
+
+
+
         // Prepare the comparison div
         const div_pair = d3.select("#pair-graphs-div");
         div_pair.attr("opacity",0);
+
+        // Set x and y scales
+        let x_scale = d3.scaleLinear()
+            .domain([0, 1])
+            .range([50, svg_pair_s-50]);
+
+        
+        let y_scale = d3.scaleLinear().range([svg_pair_s-50,50]);
+        let yAxis = d3.axisLeft().scale(y_scale);
+
+        svg1.append("g")
+            .attr("class","myYaxis")
+            .attr("transform", `translate(100,0)`);
+
+
+        // Data things
+        const nEdges = 2415;
+        const betti_colors = ["#243A4C", "#406372", "#66939E", "#9BC3C6"];
 
 
 
@@ -30,105 +56,167 @@
 
 
         d3.csv("../data/test.csv", function(error, data) {
+            d3.json("../data/foo4.json", function(error, bettis) {
             
 
 
-            // UPDATE LATER
-            const model_names = ["IID","assortative","coreperiphery", "cosineGeometric","disassortative", "discreteUniform","dotProduct", "geometricConf", "randomGeometric", "ringLattice", "rmsd", "squaredEuclidean" ];
+                // UPDATE LATER
+                const model_names = ["IID","assortative","coreperiphery", "cosineGeometric","disassortative", "discreteUniform","dotProduct", "geometricConf", "randomGeometric", "ringLattice", "rmsd", "squaredEuclidean" ];
+                const model_abbrevs = ["IID", "ast", "cp", "cosG", "dast", "disU", "dp", "geom","rg", "rl", "rmsd", "se"];
 
-            const nModels = Math.sqrt(data.length);
-            const box_s = 35;
+                const nModels = Math.sqrt(data.length);
+                const box_s = 35;
 
-            let position_scale = model_names.map((e,i) => [e, axis_scale(i)])
-            console.log(position_scale)
-            let position_map = new Map(position_scale);
-            console.log(position_map)
-
-
-            // Pick one threshold, create matrix
-            let visible_threshold = "thresh05";
-
-            // Draw boxes
-            let boxes = svg.append("g")
-            boxes.selectAll("g")
-                .data(data)
-                .enter()
-                .append("rect")
-                    .attr("x",function(d) {return position_map.get(d.TP.split("_")[1]);})
-                    .attr("y",function(d) {return position_map.get(d.TP.split("_")[0]);})
-                    .attr("width", box_s)
-                    .attr("height",box_s)
-                    .attr("fill",function(d) {return (d.TP.split("_")[0] === d.TP.split("_")[1]) ? "teal" : "pink";})
-                    .attr("stroke","white")
-                    .attr("stroke-width", 0.5)
-                    .attr("stroke-opacity", 0.5)
-                    .style("fill-opacity", function(d) {console.log(d[visible_threshold]/100); return d[visible_threshold]/100})
-                    .on("mouseover", mouseover)
-                    .on("mouseout", mouseout);
-
-            // Write names
-            // REPLACE WITH D3 AXES
-            const predicted_names = svg.append("g")
-            predicted_names.selectAll("text")
-                .data(model_names)
-                .enter()
-                .append("text")
-                    .text(function(d) {return d})
-                    .attr("fill", "white")
-                    .attr("x",function(d) {return position_map.get(d) + box_s/2;})
-                    .attr("y",95)
-                    .attr("text-anchor","middle");
-
-            const correct_names = svg.append("g")
-            correct_names.selectAll("text")
-                .data(model_names)
-                .enter()
-                .append("text")
-                    .text(function(d) {return d})
-                    .attr("fill", "white")
-                    .attr("y",function(d) {return position_map.get(d)+box_s/2;})
-                    .attr("x",95)
-                    .attr("text-anchor","end");
+                let position_scale = model_names.map((e,i) => [e, axis_scale(i)])
+                console.log(position_scale)
+                let position_map = new Map(position_scale);
+                console.log(position_map)
 
 
-            function mouseover(d) {
+                // Pick one threshold, create matrix
+                let visible_threshold = "thresh05";
+                let visible_edge = "1208";
 
-                // d3.select(this).transition().duration(50).attr("stroke-width",2)
-                d3.select(this).transition().duration(50).attr("stroke-opacity",1).attr("stroke-width", 2);
+                // Draw boxes
+                let boxes = svg.append("g")
+                boxes.selectAll("g")
+                    .data(data)
+                    .enter()
+                    .append("rect")
+                        .attr("x",function(d) {return position_map.get(d.TP.split("_")[1]);})
+                        .attr("y",function(d) {return position_map.get(d.TP.split("_")[0]);})
+                        .attr("width", box_s)
+                        .attr("height",box_s)
+                        .attr("fill",function(d) {return (d.TP.split("_")[0] === d.TP.split("_")[1]) ? "teal" : "pink";})
+                        .attr("stroke","white")
+                        .attr("stroke-width", 0.5)
+                        .attr("stroke-opacity", 0.5)
+                        .style("fill-opacity", function(d) {console.log(d[visible_threshold]/100); return d[visible_threshold]/100})
+                        .on("mouseover", mouseover)
+                        .on("mouseout", mouseout)
+                        .on("click",onclick);
 
-                // Make the div visible
-                div_pair.transition()
-                .duration('50')
-                .style("opacity", 1)
-            };
+                // Write names
+                // REPLACE WITH D3 AXES
+                const predicted_names = svg.append("g")
+                predicted_names.selectAll("text")
+                    .data(model_abbrevs)
+                    .enter()
+                    .append("text")
+                        .text(function(d) {return d})
+                        .attr("fill", "white")
+                        .attr("x",function(d,i) {return position_map.get(model_names[i]) + box_s/2;})
+                        .attr("y",95)
+                        .attr("text-anchor","middle");
 
-            function mouseout(d) {
-
-                d3.select(this).transition().duration(50).attr("stroke-opacity",0.5).attr("stroke-width", 0.5);
-
-
-            }
-
-
-            
-
-
-
-
-
-
-            
-
-
-
+                const correct_names = svg.append("g")
+                correct_names.selectAll("text")
+                    .data(model_abbrevs)
+                    .enter()
+                    .append("text")
+                        .text(function(d) {return d})
+                        .attr("fill", "white")
+                        .attr("y",function(d,i) {return position_map.get(model_names[i])+box_s/2;})
+                        .attr("x",95)
+                        .attr("text-anchor","end");
 
 
+                function mouseover(d) {
+
+                    // d3.select(this).transition().duration(50).attr("stroke-width",2)
+                    d3.select(this).transition().duration(50).attr("stroke-opacity",1).attr("stroke-width", 2);
+
+                };
+
+                function mouseout(d) {
+
+                    d3.select(this).transition().duration(50).attr("stroke-opacity",0.5).attr("stroke-width", 0.5);
+
+
+                };
+
+                function onclick(d) {
+
+                    // Filter the data to contain only columns we care about
+                    console.log(bettis[d.TP.split("_")[0]][visible_edge])
+                    bettis1 = bettis[d.TP.split("_")[0]][visible_edge];
+                    bettis2 = bettis[d.TP.split("_")[1]][visible_edge];
+
+                    let lines1 = {line1: svg1.selectAll(".dim1_t").data([bettis1.dim1]),
+                    line2: svg1.selectAll(".dim2_t").data([bettis1.dim2]),
+                    line3: svg1.selectAll(".dim3_t").data([bettis1.dim3]),
+                    line4: svg1.selectAll(".dim4_t").data([bettis1.dim4])};
+
+                    let lines2 = {line1: svg2.selectAll(".dim1_p").data([bettis2.dim1]),
+                    line2: svg2.selectAll(".dim2_p").data([bettis2.dim2]),
+                    line3: svg2.selectAll(".dim3_p").data([bettis2.dim3]),
+                    line4: svg2.selectAll(".dim4_p").data([bettis2.dim4])};
+
+                    // Adjust y axis
+                    let max_y = d3.max([d3.max(bettis1.dim1), d3.max(bettis1.dim2), d3.max(bettis1.dim3), d3.max(bettis1.dim4),
+                        d3.max(bettis2.dim1), d3.max(bettis2.dim2), d3.max(bettis2.dim3), d3.max(bettis2.dim4)]);
+
+                    let buffer = max_y*(0.1);
+
+
+                    
+                    y_scale.domain([0, max_y+buffer]);
+
+                    for (let index = 0; index < 4; index++) {
+
+                                    lines1[`line${index+1}`].enter()
+                                        .append("path")
+                                            .attr("class","dims")
+                                            .attr("class",`dim${index+1}_t`)
+                                            .merge(d3.selectAll(`.dim${index+1}_t`))
+                                            .transition()
+                                            .duration(1000)
+                                            .attr("d", d3.line()
+                                                .x(function(d, i) {return x_scale(i/nEdges); })
+                                                .y(function(d) {return y_scale(d); }))
+                                            .attr("stroke", betti_colors[index])
+                                            .attr("fill", "none")
+                                            .attr("stroke-width", 3);
+
+
+
+                                    lines2[`line${index+1}`].enter()
+                                        .append("path")
+                                            .attr("class","dims")
+                                            .attr("class",`dim${index+1}_p`)
+                                            .merge(d3.selectAll(`.dim${index+1}_p`))
+                                            .transition()
+                                            .duration(1000)
+                                            .attr("d", d3.line()
+                                                .x(function(d, i) {return x_scale(i/nEdges); })
+                                                .y(function(d) {return y_scale(d); }))
+                                            .attr("stroke", betti_colors[index])
+                                            .attr("fill", "none")
+                                            .attr("stroke-width", 3);
+
+                    }
+
+
+                    
+
+
+                    // Make the div visible
+                    div_pair.transition()
+                    .duration('50')
+                    .style("opacity", 1)
+
+
+                };
+
+                console.log(bettis)
 
 
 
 
 
 
+
+            }) 
         })
 
 

56ad5bacdf1fb806b517436815d3512af7bb96fc
Ann Sizemore <annsize@seas.upenn.edu>
2020-12-10 18:37:08 -0500

    fixed dropdown text

--- js/single_clique.js
+++ js/single_clique.js
@@ -21,6 +21,8 @@
         const real_color = "#B48677";
         const noise_color = "#BFA658";
 
+        // const model_names = ["20-clique", "clique", "30-clique", "5-clique", "10-clique", "2-clique", "35-clique", ""]
+
 
 
 
@@ -34,6 +36,12 @@
 
             const models = Object.keys(dict)
             console.log(models)
+            const models_clean = models.map(model => {
+                let k = model.split("lique")[1];
+                return k ? (k+"-clique") : "clique"});
+
+
+
             console.log("hello")
 
             let model = "clique"
@@ -305,7 +313,9 @@
                 .enter().append("option")
                 .attr("value", function (d) { return d; })
                 .text(function (d) {
-                    return d[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
+                    // return d[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
+                    let k = d.split("lique")[1];
+                    return k ? (k+"-clique") : "Clique"
                 });
 
             dropdown.append("text")
